63004cb6fd85e2861be8631141f53e0a3380413e
JayTeeSF <jonathan@his-service.net>
2012-08-25 22:39:58 -0700

    typo-fix

--- lib/fortune_teller/list_renderer.rb
+++ lib/fortune_teller/list_renderer.rb
@@ -1,11 +1,13 @@
-class ListRenderer
-  attr_reader :separator
-  def initialize( options = {} )
-    @separator = options[:separator] || "\n"
-  end
+module FortuneTeller
+  class ListRenderer
+    attr_reader :separator
+    def initialize( options = {} )
+      @separator = options[:separator] || "\n"
+    end
 
-  def render( text_array )
-    "Pick one (i.e. type it, then press enter):\n" +
-      text_array.map(&:to_s).join( separator ) + "\n"
+    def render( text_array )
+      "Pick one (i.e. type it, then press enter):\n" +
+        text_array.map(&:to_s).join( separator ) + "\n"
+    end
   end
 end
